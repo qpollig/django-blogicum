2374575b218f454a8ab10b48cd812f29a268e862
Polina Nemechkina <qpollig@gmail.com>
2024-05-10 17:25:43 +0500

    Error Pages

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -10,3 +10,8 @@ urlpatterns = [
     path('category/<slug:category_slug>/', views.category_posts,
          name='category_posts'),
 ]
+
+
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
