c89eba0e5fff7b07d96ca7ef14e833a5036d0e59
Polina Nemechkina <qpollig@gmail.com>
2024-05-25 12:17:47 +0500

    HTTPStatus

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from http import HTTPStatus
 
 
 def page_not_found(request, exception):
-    return render(request, 'pages/404.html', status=404)
+    return render(request, 'pages/404.html', status=HTTPStatus.NOT_FOUND)
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'pages/403csrf.html', status=403)
+    return render(request, 'pages/403csrf.html', status=HTTPStatus.FORBIDDEN)
 
 
 def server_error(request, reason=''):
-    return render(request, 'pages/500.html', status=500)
+    return render(request, 'pages/500.html', status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
 
 class AboutView(TemplateView):
